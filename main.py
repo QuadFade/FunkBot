4f3064fd7a7493cb22736ba57ddbd0241743b8d3
John Fade IV <John.fadeiv@gmail.com>
2022-01-21 15:22:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("ODkwNjI1MDg0ODI0MzU5MDY0.YUyhPw.tTw3gmENQsZ7_8UhN6xg3oGvufM")
+client.run("Discord Bot Generated Token Here")
